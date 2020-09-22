5c70225a669cb1cc9b9e994de6c86da1dc7cbf2c
unknown <tompsherman@gmail.com>
2020-09-22 12:43:39 -0400

    password update to 6 chars

--- expat-journal/src/formSchemas/regFormSchema.js
+++ expat-journal/src/formSchemas/regFormSchema.js
@@ -3,6 +3,6 @@ import * as yup from "yup"
 export default yup.object().shape({
     username: yup.string().required("username is required"),
     email: yup.string().email().required("email is required"),
-    password: yup.string().required("password is required").min(8, "password must be 8 characters long"),
+    password: yup.string().required("password is required").min(6, "password must be 6 characters long"),
     checkbox: yup.boolean().oneOf([true], "you must agree to terms of service")
 })
